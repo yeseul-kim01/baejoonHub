31a1915b748045fa28ff10104b1dc414dae37737
yeseul-kim01 <yesul0718@naver.com>
2025-01-11 21:17:19 +0200

    [Gold V] Title: 상범 빌딩, Time: 200 ms, Memory: 35016 KB -BaekjoonHub

---
+++ 백준/Gold/6593. 상범 빌딩/상범 빌딩.py
@@ -0,0 +1,51 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+dz = [1, -1, 0, 0, 0, 0]
+dx = [0, 0, 1, -1, 0, 0]
+dy = [0, 0, 0, 0, 1, -1]
+
+
+while True:
+    l, r, c = map(int, input().split())
+    if l == 0 and r == 0 and c == 0:
+        break
+    board = []
+    visit = [[[False] * c for _ in range(r)] for _ in range(l)]
+    for _ in range(l):
+        board.append([list(input().strip()) for _ in range(r)])
+        temp = input()
+
+    q = deque()
+    escaped = False
+
+    for i in range(l):
+        for j in range(r):
+            for k in range(c):
+                if board[i][j][k] == 'S':
+                    start = (i, j, k, 0)
+                    visit[i][j][k] = True
+                if board[i][j][k] == 'E':
+                    goal = (i, j, k)
+
+    q.append(start)
+    while q:
+        z, x, y, d = q.popleft()
+        if (z, x, y) == goal:
+            escaped = True
+            print(f'Escaped in {d} minute(s).')
+            break
+        nd = d + 1
+
+        for i in range(6):
+            nz = z + dz[i]
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nz < l and 0 <= nx < r and 0 <= ny < c and not visit[nz][nx][ny]:
+                if board[nz][nx][ny] == '.' or board[nz][nx][ny] == 'E':
+                    q.append((nz, nx, ny, nd))
+                    visit[nz][nx][ny] = True
+
+    if not escaped:
+        print('Trapped!')
