eb7b7a5a53940bc11fae3210036d698d8efee902
yeseul-kim01 <yesul0718@naver.com>
2025-01-17 01:58:44 +0900

    [Silver III] Title: N과 M (1), Time: 188 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,16 @@
+N, M = map(int,input().split())
+ans = []
+
+def back():
+    if len(ans) == M:
+        print(*ans)
+        return
+    for i in range(1,N+1):
+        if i not in ans:
+            ans.append(i)
+            back()
+            ans.pop()
+            
+back()
+            
+        
