6e306443949ad8b29807f775f9a18a8b6fae7776
yeseul-kim01 <yesul0718@naver.com>
2025-01-19 20:37:55 +0900

    [Silver II] Title: N과 M (9), Time: 88 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -0,0 +1,21 @@
+n,m = map(int,input().split())
+s = list((map(int,input().split())))
+ans = []
+visit = [False]*n
+s.sort()
+
+def back(x):
+    if len(ans) == m:
+        print(*ans)
+        return
+    value = -1
+    for i in range (n):
+        if visit[i] != True and s[i] != value:
+            value = s[i]
+            ans.append(s[i])
+            visit[i] = True
+            back(i+1)
+            visit[i] = False
+            ans.pop()
+
+back(0)
