3c146f9b667e2e52e4dd427a354d7d8a93e0faec
yeseul <yesul0718@naver.com>
2025-10-29 19:56:47 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 10.34 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,35 +1,62 @@
+import java.util.Arrays;
+
 class Solution {
     public String solution(String s) {
-        String answer = "";
+//         String answer = "";
+//         // 공백으로 문자열 나눠서 문자열 배열에 담기
+//         String[] parts = s.split(" ");
         
-        // 공백으로 문자열 나눠서 문자열 배열에 담기
-        String[] parts = s.split(" ");
+//         // 문자열 배열의 길이만큼 정수 배열 만들기
+//         int[] numbers = new int[parts.length];
         
-        // 문자열 배열의 길이만큼 정수 배열 만들기
-        int[] numbers = new int[parts.length];
-        
-        // 문자열 하나씩 정수로 바꿔서 numbers에 저장하기
-        for (int i =0; i<parts.length; i++) {
-            // Integer.parseInt (변환)
-            numbers[i] = Integer.parseInt(parts[i]);
-        }
-        
-        // min max 처음 값으로 초기화하기
-        int min = numbers[0];
-        int max = numbers[0];
+//         // 문자열 하나씩 정수로 바꿔서 numbers에 저장하기
+//         for (int i =0; i<parts.length; i++) {
+//             // Integer.parseInt (변환)
+//             numbers[i] = Integer.parseInt(parts[i]);
+//         }
+//         // min max 처음 값으로 초기화하기
+//         int min = numbers[0];
+//         int max = numbers[0];
             
-        for (int num : numbers) {
-            if (min>num) {
-                min = num;
+//         for (int num : numbers) {
+//             if (min>num) {
+//                 min = num;
+//             }
+//             if (max<num) {
+//                 max = num;
+//             }
+//         }
+        
+//         StringBuffer sb = new StringBuffer();
+//         sb.append(Arrays.stream(numbers).min().getAsInt());
+//         sb.append(" ");
+//         sb.append(Arrays.stream(numbers).max().getAsInt());
+        
+//         return sb.toString();
+//     }
+// }
+        
+// //         // String.valueOf == Integer.toString
+// //         answer += String.valueOf(min) +" " +Integer.toString(max);
+        
+// //         return answer;
+        
+        String [] ar = s.split(" ");
+        int [] arrInt = new int[ar.length];
+        int max,min;
+        max = min = Integer.parseInt(ar[0]);
+        for (String parts:ar) {
+            int partsInt = Integer.parseInt(parts);
+            if (partsInt<min) {
+                min = partsInt; 
             }
-            if (max<num) {
-                max = num;
+            
+            if (partsInt>max)
+            {
+                max = partsInt;
             }
         }
         
-        // String.valueOf == Integer.toString
-        answer += String.valueOf(min) +" " +Integer.toString(max);
-        
-        return answer;
+        return min + " " + max;
     }
 }
