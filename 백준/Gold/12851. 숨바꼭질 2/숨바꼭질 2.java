bd70be1e902f94974b3781bb1ca80d2e48e8bfc3
yeseul <yesul0718@naver.com>
2026-03-07 23:25:51 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 200 ms, Memory: 31416 KB -BaekjoonHub

---
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader ( new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] visited = new int[100001]; // 시간 저장
+        int[] count = new int[100001]; //개수
+        Arrays.fill(visited,-1); // -1 은 방문 안한거
+        visited[N] = 0 ; //처음 시작 값
+        Arrays.fill(count,0); // 카운트 개수
+        count[N] = 1; // 처음 시작 경로값
+        Deque<Integer> queue = new ArrayDeque<>(); 
+        queue.add(N); // 처음 시작 값 넣기 
+        int cur;
+
+        while(!queue.isEmpty()){
+            cur = queue.poll(); // 하나씩 제거하기 
+            if (cur == M){
+                //목표값 이라면
+                break;
+            }
+            // System.out.println("현재 cur 값:"+cur);
+            List<Integer> list = Arrays.asList(cur + 1, cur - 1, cur * 2); //TODO: 값이 있는 리스트 만들기 확인 
+            for (int i : list){
+                if ( i <0 || i>100000) {
+                    continue; // 벗어나면 그다음꺼로 넘어감 
+                }
+                // System.out.println("현재 확인 중인 값:"+ i);
+                if (visited[i] == -1) {
+                    visited[i] = visited[cur] +1; // 시간 업데이트 
+                    count[i] = count[cur]; // 같은 경로니 카운트 유지시키기
+                    queue.add(i);
+                }
+                else if (visited[i] == visited[cur]+1) {
+                    // 서로 같다면 카운트를 증가시키기 
+                    // 시간은 같은데 
+                    count[i] += count[cur]; // 길이 하나 더 있는 거니까... 
+                }
+                // System.out.println("확인 마침:"+ i + "count:"+count[i]);
+            }
+        }
+        System.out.println(visited[M]);
+        System.out.println(count[M]);
+
+
+
+    }
+}
